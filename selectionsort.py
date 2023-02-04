307202d7ba0d595f00cd143f38e107b8d586b0a7
renwish <eugenebasinio@gmail.com>
2023-02-05 04:04:53 +0800

    done making sort, making temporary variable for swapping

--- selectionsort.py
+++ selectionsort.py
@@ -1,4 +1,11 @@
 def sort(numbers):
 
     for i in range(99):
-    minpos = i
+        minpos = i
+
+        for j in range(i,100):
+            if numbers[j] < numbers[minpos]:
+                minpos = j
+        
+        swap = numbers[i]
+
