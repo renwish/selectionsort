2d8002fbaf8813b00c44f3bd05d1d60424d20433
renwish <eugenebasinio@gmail.com>
2023-02-05 04:14:48 +0800

    fixed last bug, understood the range in loops

--- selectionsort.py
+++ selectionsort.py
@@ -1,15 +1,17 @@
 def sort(numbers):
 
-    for i in range(99):
+    for i in range(9):
         minpos = i
 
-        for j in range(i,100):
+        for j in range(i,10):
             if numbers[j] < numbers[minpos]:
                 minpos = j
         
         swap = numbers[i]
         numbers[i] = numbers[minpos]
         numbers[minpos] = swap
+
+        print(numbers)
 
 numbers = [77, 93, 56, 98, 41, 17, 80, 7, 39, 57]
 sort(numbers)
